9b7242f50c459c28a98b59ef426c550f4ba3f026
jodar <zodarbekn@gmail.com>
2022-10-20 19:19:55 +0300

    dop4

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
-
 let title = document.getElementsByTagName('h1')
 let inp1 = document.getElementById('one')
 let inp2 = document.getElementById('two')
 let btn = document.getElementById('switch')
 
-console.log(inp1);
-console.log(inp2);
-console.log(btn);
-console.log(title[0]);
+let values;
 
-document.getElementById('switch').onclick = () => {
-    [
-        document.getElementById('one').value,
-        document.getElementById('two').value
-    ] = [
-        document.getElementById('two').value,
-        document.getElementById('one').value
-    ]
+function change () {
+    values = inp1.value
+    inp1.value = inp2.value
+    inp2.value = values
 }
+
+btn.addEventListener('click', () => {
+    change()
+})
